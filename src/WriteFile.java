9ded538d0b16de1a29b600473c6b6c4784dc070b
khalefa <khalefa@ubuntu.hsd1.mn.comcast.net>
2011-02-21 05:40:07 -0600

    Spatial operation with Map only

--- src/WriteFile.java
+++ src/WriteFile.java
@@ -1,24 +1,35 @@
-import java.awt.Rectangle;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.LineNumberReader;
-
+import java.io.*;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 public class WriteFile {
-
-	public static final String theFilename = "/hello.dat";
+	public static  String  HIST_FILENAME = "histgram.txt";
+    public static int [][]histogram;
+     
+	public static final String theFilename = "/hello.txt";
 	private static final int BlockSize = 64 * 1024 * 1024;
-
+	private static final int X= 1024;
+	private static final int Y= 1024;
+	private static final int GRID_X = 128;
+	private static final int GRID_Y = 128;
+	private static final int N_X = X/GRID_X;
+	private static final int N_Y = Y/GRID_Y;
+	
 	public static void main (String [] args) throws IOException {
-
+		
+		histogram = new int [N_X][N_Y];
+		for(int i=0;i<N_X;i++){
+			for(int j=0;j<N_Y;j++){
+				histogram[i][j] = 0;
+		     }   
+	     }
      Configuration conf = new Configuration();
-     conf.set("fs.default.name", "hdfs://localhost:9000");
-     conf.set("dfs.data.dir", "/home/eldawy/hdfs/data");
-     conf.set("dfs.name.dir", "/home/eldawy/hdfs/name");
+     conf.set("fs.default.name", "hdfs://localhost:54310");
+     conf.set("dfs.data.dir", "/home/khalefa/hadoop-khalefa/dfs/data");
+     conf.set("dfs.name.dir", "/home/khalefa/hadoop-khalefa/dfs/name");
      FileSystem fs = FileSystem.get(conf);
 
      Path filenamePath = new Path(theFilename);
@@ -30,12 +41,13 @@ public class WriteFile {
        }
 
        FSDataOutputStream out = fs.create(filenamePath);
-       for (int cy1 = 0; cy1 < 1024; cy1 += 256) {
-    	   for (int cx1 = 0; cx1 < 1024; cx1 += 256) {
-    		   Rectangle cell = new Rectangle(cx1, cy1, cx1 + 256, cy1 + 256);
+       for (int cy1 = 0; cy1 < X; cy1 += GRID_X) {
+    	   for (int cx1 = 0; cx1 < Y; cx1 += GRID_Y) {
+    		   int cx2 = cx1 + GRID_X;
+    		   int cy2 = cy1 + GRID_Y;
     		   long bytesSoFar = 0;
     		   
-    		   LineNumberReader reader = new LineNumberReader(new FileReader("test.txt"));
+    		   LineNumberReader reader = new LineNumberReader(new FileReader("/home/khalefa/Desktop/test.txt"));
     		   while (reader.ready()) {
     			   String line = reader.readLine();
     			   String[] parts = line.split(",");
@@ -43,17 +55,16 @@ public class WriteFile {
     			   int py1 = Integer.parseInt(parts[2]);
     			   int px2 = Integer.parseInt(parts[3]);
     			   int py2 = Integer.parseInt(parts[4]);
-    			   Rectangle r = new Rectangle(px1, py1, px2, py2);
-    			   if (r.intersects(cell)) {
-    				   // This rectangle belongs to this cell and should be written
-    				   byte[] data = line.getBytes("ASCII");
-    				   // Write leading zeros to be sure block size is 32
-    				   for (int i = 0; i < (32 - data.length); i++) {
-    					   out.writeByte('0');
-    					   bytesSoFar++;
+    			   
+    			   if (!(px1 > cx2 || px2 < cx1)) {
+    				   if (!(py1 > cy2 || py2 < cy1)) {
+    					   // This rectangle belongs to this cell and should be written
+    					   int x_i = cx1/GRID_X;
+    					   int y_i = cy1/GRID_Y;
+    					   histogram[x_i][y_i]++;        
+    					   out.writeUTF(line);
+    					   bytesSoFar += line.getBytes("utf-8").length + 2;
     				   }
-    				   out.write(data);
-    				   bytesSoFar += data.length;
     			   }
     		   }
     		   reader.close();
@@ -68,10 +79,22 @@ public class WriteFile {
        }
        out.close();
 
-       /*FSDataInputStream in = fs.open(filenamePath);
+       FSDataInputStream in = fs.open(filenamePath);
        String messageIn = in.readUTF();
        System.out.print(messageIn);
-       in.close();*/
+       in.close();
+       System.out.print("\n");
+       System.out.print("\n");
+       DataOutputStream os = new DataOutputStream(new FileOutputStream(HIST_FILENAME));
+       for(int i=0;i< N_X; i++) {
+    	   for(int j=0;j< N_Y; j++){
+    		System.out.print(histogram[i][j]+" ");   
+             os.writeInt(histogram[i][j]);
+    	   }
+    	   System.out.print("\n");
+    	   }
+       os.close();
+       
      } catch (IOException ioe) {
        System.err.println("IOException during operation: " + ioe.toString());
        System.exit(1);
