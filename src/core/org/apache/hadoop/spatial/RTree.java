e665bf7dd53f182e072655ec16b5560f11c3f195
Ahmed Eldawy <eldawy@cs.umn.edu>
2012-10-05 17:46:16 -0500

    Refactor RTree to simplify the searchDisk function given that all nodes have the same size on disk

--- src/core/org/apache/hadoop/spatial/RTree.java
+++ src/core/org/apache/hadoop/spatial/RTree.java
@@ -1,26 +1,25 @@
 package org.apache.hadoop.spatial;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInput;
 import java.io.DataInputStream;
 import java.io.DataOutput;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Random;
-import java.util.Set;
 import java.util.Stack;
 import java.util.Vector;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.util.IndexedSortable;
 import org.apache.hadoop.util.QuickSort;
@@ -107,12 +106,9 @@ public class RTree<T extends Shape> implements Writable {
   /**Size of tree header on disk. Height + Degree + Number of records*/
   private static final int TreeHeaderSize = 4 + 4 + 4;
 
-  /**Size of a non-leaf node. Four dimensions (x, y, width, height)*/
-  private static final int NonLeafNodeSize = 8 * 4;
+  /**Size of a node. Offset of first child + dimensions (x, y, width, height)*/
+  private static final int NodeSize = 4 + 8 * 4;
 
-  /**Size of a leaf node. We add offset of the first element in file*/
-  private static final int LeafNodeSize = NonLeafNodeSize + 4;
-  
   /// The root node for the tree. null if the tree is empty.
   Node root;
   
@@ -256,73 +252,100 @@ public class RTree<T extends Shape> implements Writable {
   public void write(DataOutput out) throws IOException {
     // Create a separate stream for node (structure) and elements (data)
     ByteArrayOutputStream nodeStream;
-    FSDataOutputStream nodeOut = new FSDataOutputStream(
+    FSDataOutputStream tempNodeOut = new FSDataOutputStream(
         nodeStream = new ByteArrayOutputStream(), null);
 
     ByteArrayOutputStream dataStream;
-    FSDataOutputStream dataOut = new FSDataOutputStream(
+    FSDataOutputStream tempDataOut = new FSDataOutputStream(
         dataStream = new ByteArrayOutputStream(), null);
     
     // A list of nodes to be written do disk. Nodes are written in their
     // level-order traversal
-    Queue<Node> toBeWritten = new LinkedList<Node>();
+    Queue<Node> toBeVisited = new LinkedList<Node>();
     long offsetOfFirstElement = 0;
     
+    int nodeCount;
+    int height;
+    int degree;
     if (root != null) {
-      int height = getTreeHeight();
-      int degree = root.children.size();
-      int nodeCount = degree == 1 && height == 1 ?
+      height = getTreeHeight();
+      degree = root.children.size();
+      nodeCount = degree == 1 && height == 1 ?
           1 : (int) ((Math.pow(degree, height) - 1) / (degree - 1));
-      int leafNodeCount = (int) Math.pow(degree, height - 1);
       if (nodeCount != getNodeCount())
         throw new RuntimeException("Node count "+nodeCount+" != "+getNodeCount());
       
       // Write tree header
-      nodeOut.writeInt(height);
-      nodeOut.writeInt(degree);
-      nodeOut.writeInt(getElementCount());
+      tempNodeOut.writeInt(height);
+      tempNodeOut.writeInt(degree);
+      tempNodeOut.writeInt(getElementCount());
       
       offsetOfFirstElement = TreeHeaderSize +
-          NonLeafNodeSize * (nodeCount - leafNodeCount)+
-          LeafNodeSize * (leafNodeCount);
+          NodeSize * nodeCount;
       
       // Start writing elements from the root
-      toBeWritten.add(root);
+      toBeVisited.add(root);
     } else {
-      int height = 0;
+      height = 0;
+      nodeCount = 0;
+      degree = 0;
       offsetOfFirstElement = 4;
-      nodeOut.writeInt(height); // 0 height indicates an empty tree 
+      tempNodeOut.writeInt(height); // 0 height indicates an empty tree 
     }
+    // Map each node to the offset of first child on disk.
+    // For leaves, the first child is a direct child to the node
+    // For non leaves, this offset is equal to the offset of first child for
+    // its left most child node
+    Map<Node, Integer> offsetOfFirstChild = new HashMap<Node, Integer>();
+    // Nodes that need to be written to disk
+    Vector<Node> toBeWritten = new Vector<Node>();
+    while (!toBeVisited.isEmpty()) {
+      // Get next element to be visited
+      Node n = toBeVisited.poll();
+      toBeWritten.add(n);
 
-    while (!toBeWritten.isEmpty()) {
-      // Get next element to be written
-      Node n = toBeWritten.poll();
-      
       if (!n.leaf) {
-        // Write MBR of this node
-        n.getMBR().write(nodeOut);
-
         // Serialize all child nodes
         for (Shape child : n.children) {
-          toBeWritten.add((Node)child);
+          toBeVisited.add((Node)child);
         }
       } else {
-        // For leaves. Also store the offset of the first element in this
-        // node
-        nodeOut.writeInt((int) (offsetOfFirstElement + dataOut.getPos()));
-        n.getMBR().write(nodeOut);
-        
+        // For leaves. Record the offset of the first element in this node
+        offsetOfFirstChild.put(n,
+            (int) (offsetOfFirstElement + tempDataOut.getPos()));
         // Then, write elements to data out
         for (Shape s : n.children) {
-          s.write(dataOut);
+          s.write(tempDataOut);
         }
       }
     }
-    if (nodeOut.getPos() != offsetOfFirstElement)
-      throw new RuntimeException("Actual pos: "+nodeOut.getPos()+" != Expected post "+offsetOfFirstElement);
-    long treeSize = nodeOut.getPos() + dataOut.getPos();
-    nodeOut.close();
-    dataOut.close();
+    
+    System.out.println("Going to write "+toBeWritten.size()+" nodes");
+
+    if (toBeWritten.size() != nodeCount)
+      throw new RuntimeException("Some nodes will not be written");
+    
+    // Calculate offsetOfFirstChild for all nodes (non leaf nodes)
+    for (int i = toBeWritten.size() - 1; i >= 0; i--) {
+      Node n = toBeWritten.elementAt(i);
+      if (!n.leaf) {
+        offsetOfFirstChild.put(n, offsetOfFirstChild.get(n.children.firstElement()));
+      }
+    }
+
+    // Write nodes
+    for (Node n : toBeWritten) {
+      // Write offset of first child
+      tempNodeOut.writeInt(offsetOfFirstChild.get(n));
+      n.write(tempNodeOut);
+    }
+    // Make sure our precalculations were correct
+    if (tempNodeOut.getPos() != offsetOfFirstElement)
+      throw new RuntimeException("Actual pos: "+tempNodeOut.getPos()+" != Expected post "+offsetOfFirstElement);
+    
+    long treeSize = tempNodeOut.getPos() + tempDataOut.getPos();
+    tempNodeOut.close();
+    tempDataOut.close();
     
     // Really write to the output stream
     out.writeLong(treeSize);
@@ -369,7 +392,7 @@ public class RTree<T extends Shape> implements Writable {
    * @return
    */
   public static Rectangle[] packInRectangles(GridInfo gridInfo, final Point[] sample) {
-    Rectangle[] rectangles = new Rectangle[gridInfo.getGridColumns() * gridInfo.getGridRows()];
+    Rectangle[] rectangles = new Rectangle[gridInfo.columns * gridInfo.rows];
     int iRectangle = 0;
     // Sort in x direction
     final IndexedSortable sortableX = new IndexedSortable() {
@@ -406,31 +429,33 @@ public class RTree<T extends Shape> implements Writable {
     quickSort.sort(sortableX, 0, sample.length);
 
     int xindex1 = 0;
-    for (int col = 0; col < gridInfo.getGridColumns(); col++) {
-      int xindex2 = sample.length * (col + 1) / gridInfo.getGridColumns();
+    long x1 = gridInfo.xOrigin;
+    for (int col = 0; col < gridInfo.columns; col++) {
+      int xindex2 = sample.length * (col + 1) / gridInfo.rows;
       
       // Determine extents for all rectangles in this column
-      long x1 = sample[xindex1].x;
-      long x2 = sample[xindex2-1].x;
+      long x2 = col == gridInfo.columns - 1 ? 
+          gridInfo.xOrigin + gridInfo.gridWidth : sample[xindex2-1].x;
       
       // Sort all points in this column according to its y-coordinate
       quickSort.sort(sortableY, xindex1, xindex2);
       
       // Create rectangles in this column
-      int yindex1 = xindex1;
-      for (int row = 0; row < gridInfo.getGridRows(); row++) {
+      long y1 = gridInfo.yOrigin;
+      for (int row = 0; row < gridInfo.rows; row++) {
         int yindex2 = xindex1 + (xindex2 - xindex1) *
-            (row + 1) / gridInfo.getGridRows();
-        long y1 = sample[yindex1].y;
-        long y2 = sample[yindex2-1].y;
+            (row + 1) / gridInfo.rows;
+        long y2 = row == gridInfo.rows - 1 ? 
+            gridInfo.yOrigin + gridInfo.gridHeight : sample[yindex2-1].y;
         
         rectangles[iRectangle++] = new Rectangle(x1, y1, x2-x1, y2-y1);
-        yindex1 = yindex2;
+        y1 = y2;
       }
       
       xindex1 = xindex2;
+      x1 = x2;
     }
-    return null;
+    return rectangles;
   }
 
   /**
@@ -485,6 +510,7 @@ public class RTree<T extends Shape> implements Writable {
   
   protected int searchDisk(Rectangle query, ResultCollector<T> results) {
     int resultSize = 0;
+
     // Check for an empty tree
     if (serializedTree == null)
       return 0;
@@ -500,7 +526,7 @@ public class RTree<T extends Shape> implements Writable {
       int nodeCount = (int) ((Math.pow(degree, height) - 1) / (degree - 1));
       int leafNodeCount = (int) Math.pow(degree, height - 1);
       int nonLeafNodeCount = nodeCount - leafNodeCount;
-      int elementCount = in.readInt();
+      /*int elementCount =*/ in.readInt();
 
       Queue<Integer> toBeSearched = new LinkedList<Integer>();
       toBeSearched.add(0);
@@ -510,32 +536,26 @@ public class RTree<T extends Shape> implements Writable {
       while (!toBeSearched.isEmpty()) {
         int searchNumber = toBeSearched.poll();
         int mbrsToTest = searchNumber == 0 ? 1 : degree;
-        if (searchNumber < nonLeafNodeCount){
-          long nodeOffset = NodeNumberToOffset(searchNumber, degree,
-              nonLeafNodeCount);
-          in.reset(); in.skip(nodeOffset);
 
-          // Search non-leaf (internal) nodes.
-          for (int i = 0; i < mbrsToTest; i++) {
-            mbr.readFields(in);
-            if (mbr.isIntersected(query)) {
-              toBeSearched.add((searchNumber + i) * degree + 1);
-            }
-          }
-        } else if (searchNumber < nodeCount) {
-          long nodeOffset = NodeNumberToOffset(searchNumber, degree,
-              nonLeafNodeCount);
+
+        if (searchNumber < nodeCount) {
+          long nodeOffset = TreeHeaderSize + NodeSize * searchNumber;
           in.reset(); in.skip(nodeOffset);
           int dataOffset = in.readInt();
-          // Search nodes at leaf level. Test all siblings.
+          
           for (int i = 0; i < mbrsToTest; i++) {
             mbr.readFields(in);
             int lastOffset = (searchNumber+i) == nodeCount - 1 ?
                 serializedTree.length : in.readInt();
             if (mbr.isIntersected(query)) {
-              // Search all elements in this node
-              toBeSearched.add(dataOffset);
-              toBeSearched.add(lastOffset);
+              if (searchNumber < nonLeafNodeCount) {
+                // Search child nodes
+                toBeSearched.add((searchNumber + i) * degree + 1);
+              } else {
+                // Search all elements in this node
+                toBeSearched.add(dataOffset);
+                toBeSearched.add(lastOffset);
+              }
             }
             dataOffset = lastOffset;
           }
@@ -564,33 +584,12 @@ public class RTree<T extends Shape> implements Writable {
     return resultSize;
   }
 
-  /**
-   * Transforms node number (0-based) to the offset in file where this node
-   * is stored.
-   * @param nodeNumber - 0-based index of the node in the level-order traversal
-   * @param degree - Degree of the tree. We assume all non-leaf nodes have the
-   *   same degree
-   * @param nonLeafNodeCount - Number of non-leaf nodes
-   * @return
-   */
-  private static long NodeNumberToOffset(int nodeNumber, int degree,
-      int nonLeafNodeCount) {
-  long nodeOffset = TreeHeaderSize;
-    if (nodeNumber < nonLeafNodeCount) {
-      nodeOffset += NonLeafNodeSize * nodeNumber;
-    } else {
-      nodeOffset += nonLeafNodeCount * NonLeafNodeSize;
-      nodeOffset += (nodeNumber - nonLeafNodeCount) * LeafNodeSize;
-    }
-    return nodeOffset;
-  }
-
   int knn(Point pt, int k, ResultCollector<T> results) {
     return root != null? knnMemory(pt, k, results) : knnDisk(pt, k, results);
   }
 
   int knnMemory(Point pt, int k, ResultCollector<T> results) {
-    // TODO louai
+    // TODO
     return 0;
   }
 
@@ -600,14 +599,16 @@ public class RTree<T extends Shape> implements Writable {
   
   public static void main(String[] args) throws IOException {
     long t1, t2;
+    Configuration conf = new Configuration();
+    FileSystem fs = FileSystem.getLocal(conf);
+    Path testfile = new Path("test.rtree");
     Random random = new Random();
     // Test the size of RTree serialization
     final int R = 1000000; // Total number of records
     final int d = 5; // degree fan out
     final int recordSize = 42;
-    final int nodeSize = 42;
     Rectangle mbr = new Rectangle(0, 0, 10000, 10000);
-    final Rectangle query = new Rectangle(mbr.x, mbr.y, 10000, 10000);
+    final Rectangle query = new Rectangle(mbr.x, mbr.y, 100, 100);
     
     RTree<TigerShape> rtree = new RTree<TigerShape>();
     TigerShape[] values = new TigerShape[R];
@@ -637,7 +638,7 @@ public class RTree<T extends Shape> implements Writable {
 
     // Write to disk
     t1 = System.currentTimeMillis();
-    DataOutputStream dataout = new FSDataOutputStream(new BufferedOutputStream(new FileOutputStream("test.rtree")), null);
+    FSDataOutputStream dataout = fs.create(testfile, true);
     rtree.write(dataout);
     dataout.close();
     t2 = System.currentTimeMillis();
@@ -651,7 +652,7 @@ public class RTree<T extends Shape> implements Writable {
     System.out.println("Expected node count: " + N + " Vs actua node count: " + rtree.getNodeCount());
     System.out.println("Expected RTree height: " + height + " Vs Actual RTree height: " + rtree.getTreeHeight());
 
-    long expectedSize = TreeHeaderSize + R*recordSize + N*LeafNodeSize;
+    long expectedSize = TreeHeaderSize + R*recordSize + N*NodeSize;
     long actualSize = new File("test.rtree").length();
     System.out.println("Expected size: "+expectedSize+" Vs Actual size: "+actualSize);
     System.out.println("Overhead than expected: "+ 
@@ -665,7 +666,7 @@ public class RTree<T extends Shape> implements Writable {
     RTree<TigerShape> rtree2 = new RTree<TigerShape>();
     rtree2.setStockObject(new TigerShape(new Rectangle(), 1));
     
-    DataInputStream datain = new DataInputStream(new BufferedInputStream(new FileInputStream("test.rtree")));
+    FSDataInputStream datain = fs.open(testfile);
     rtree2.readFields(datain);
     datain.close();
     
@@ -678,6 +679,5 @@ public class RTree<T extends Shape> implements Writable {
     t2 = System.currentTimeMillis();
     System.out.println("Time for a small query: "+(t2-t1)+" millis");
     System.out.println("Selectivity: "+resultCount+" ("+((double)resultCount/R)+")");
-    
   }
 }
