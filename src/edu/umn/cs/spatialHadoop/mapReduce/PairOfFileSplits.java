ab6b6fc5afa2fc598cca78de254550cd004965cd
Ahmed Eldawy <eldawy@cs.umn.edu>
2012-11-07 19:16:39 -0600

    Changed PairOfFileSplits to return locations in order

--- src/edu/umn/cs/spatialHadoop/mapReduce/PairOfFileSplits.java
+++ src/edu/umn/cs/spatialHadoop/mapReduce/PairOfFileSplits.java
@@ -1,7 +1,9 @@
 package edu.umn.cs.spatialHadoop.mapReduce;
 
 import java.io.IOException;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapred.FileSplit;
@@ -24,11 +26,37 @@ public class PairOfFileSplits extends PairWritable<FileSplit> implements InputSp
   }
 
   public String[] getLocations() throws IOException {
-    HashSet<String> combinedLocations = new HashSet<String>();
-    for (String location : first.getLocations())
-      combinedLocations.add(location);
-    for (String location : second.getLocations())
-      combinedLocations.add(location);
-    return combinedLocations.toArray(new String[combinedLocations.size()]);
+    Map<String, Integer> locationFrequency = new HashMap<String, Integer>();
+    Vector<String> locations = new Vector<String>();
+    for (String location : first.getLocations()) {
+      if (!locations.contains(location))
+        locations.add(location);
+      if (locationFrequency.containsKey(location)) {
+        locationFrequency.put(location, locationFrequency.get(location) + 1);
+      } else {
+        locationFrequency.put(location, 1);
+      }
+    }
+    for (String location : second.getLocations()) {
+      if (!locations.contains(location))
+        locations.add(location);
+      if (locationFrequency.containsKey(location)) {
+        locationFrequency.put(location, locationFrequency.get(location) + 1);
+      } else {
+        locationFrequency.put(location, 1);
+      }
+    }
+    // Order by frequency to move location with may frequencies to the front
+    for (int i = 0; i < locations.size(); i++) {
+      for (int j = 0; j < locations.size() - i - 1; j++) {
+        if (locationFrequency.get(locations.elementAt(j)) <
+            locationFrequency.get(locations.elementAt(j+1))) {
+          String temp = locations.elementAt(j);
+          locations.set(j, locations.elementAt(j+1));
+          locations.set(j+1, temp);
+        }
+      }
+    }
+    return locations.toArray(new String[locations.size()]);
   }
 }
